3d17f3cebe8c50d6abfbf68cc79f95ca1c36a828
WindowBender <justcallitaday@outlook.com>
2023-07-02 17:10:06 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#include <cstdlib>
 #include <iostream>
 
-int main()
+#include "../include/Game.h"
+#include "../include/Ball.h"
+
+int main(int argc, char* argv[])
 {
-    
-    return 0;
+    std::srand(static_cast<unsigned>(time(NULL)));
+
+    // sets ball position to half the screen width then half the size of the ball as origin = 0,0
+    Ball ball ((1920.f * 0.7f)/2.f - 5, 0.f);
+    Player1Paddle p1Paddle ((1920.f * 0.7f)/ 1.013f, (1080.f * 0.7f) / 2.f - 150.f);
+    Game game (ball, p1Paddle);
 }
